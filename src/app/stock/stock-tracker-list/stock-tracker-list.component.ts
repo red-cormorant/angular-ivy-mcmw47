17281c675651ed76e1a312fb34e5390bf392fd5e
red-cormorant <59206821+red-cormorant@users.noreply.github.com>
2022-10-27 10:11:26 +0300

    2 files modified, 1 added

--- src/app/stock/stock-tracker-list/stock-tracker-list.component.ts
+++ src/app/stock/stock-tracker-list/stock-tracker-list.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
   selector: 'app-stock-tracker-list',
   templateUrl: './stock-tracker-list.component.html',
-  styleUrls: ['./stock-tracker-list.component.css']
+  styleUrls: ['./stock-tracker-list.component.css'],
 })
 export class StockTrackerListComponent implements OnInit {
+  private readonly STOCK_KEY = 'stocks';
 
-  constructor() { }
+  constructor(private localStorageService: LocalStorageService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public trackSymbol(stockId: string) {
+    this.localStorageService.setItemToLocalStorage(this.STOCK_KEY, stockId);
   }
-
-  public trackSymbol
-
 }
